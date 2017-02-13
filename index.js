f705d177f5105e21245b6a84c4844b08a6a36706
Ankit <tcs.unitedairlines@gmail.com>
2017-02-13 17:42:16 +0530

    Port Change

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+var port = process.env.port || 3000;
+
+http.listen(port, function(){
   console.log('listening on *:3000');
 });
